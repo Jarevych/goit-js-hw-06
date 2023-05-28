82b66f1abaceb815c25e47010becbca3f91e094c
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-05-28 23:03:06 +0300

    fix 9th

--- js/task-09.js
+++ js/task-09.js
@@ -3,7 +3,7 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-console.log(getRandomHexColor())
+
 const bodyEl = document.querySelector('body')
 const spanClrEl = document.querySelector('.color')
 const changeClrBtn = document.querySelector('button');
