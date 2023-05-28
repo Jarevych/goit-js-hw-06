604e39765dfb19849228facc4a364ebd2a781646
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-05-28 22:55:04 +0300

    fix 6-th

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,15 @@
 const inputEl = document.getElementById('validation-input');
 
 
-const dataLengthValue = inputEl.dataset.length
+const dataLengthValue = Number(inputEl.dataset.length)
 inputEl.addEventListener('blur', (e) => {
-    e.target.value.length <= dataLengthValue ? e.target.style.outline = "3px solid #f44336" :  e.target.style.outline = "3px solid #4caf50" 
+    if (e.target.value.length !== dataLengthValue) {
+        inputEl.classList.add('invalid');
+        inputEl.classList.remove('valid');
+    } else
+     {
+      inputEl.classList.add('valid');
+      inputEl.classList.remove('invalid');
+    };
 });
 
