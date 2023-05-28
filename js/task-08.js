8c8553f6680f1943b8a32af91856b73423771934
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-05-28 13:27:36 +0300

    8-th task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
 
+const loginFormEl = document.querySelector('.login-form');
+const btnEl = document.querySelector('button')
+
+loginFormEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const {
+        elements: { email, password }
+      } = e.currentTarget;
+    
+      if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!")}
+      else {
+       return console.log(`Login: ${email.value}, Password: ${password.value}`)};
+       e.currentTarget.reset();
+    })
